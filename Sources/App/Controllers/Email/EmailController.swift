906ae1cc825a37493592e4d37b0138868d311b5f
Johnny Nguyen <jnguyen1236@gmail.com>
2017-10-06 12:09:48 -0400

    #7: Created login route and fixed up the verification html file

--- Sources/App/Controllers/Email/EmailController.swift
+++ Sources/App/Controllers/Email/EmailController.swift
@@ -24,60 +24,14 @@ public class EmailController {
 		self.host = try config.get("host")
 	}
 	
-	public func sendResetPasswordEmail(to user: User) throws {
-		let template = HTMLTemplate.reset(user: user)
-		let message = try HTMLTemplate.makeMessage(template)
-		let email = Email(emails: [user.email], subject: template.subject, message: message)
-		
-		var emailJSON = try email.makeJSON()
-		try emailJSON.set("html", message)
-		
-		var json = JSON()
-		try json.set("recipients", try email.makeEmailJSON())
-		try json.set("content", emailJSON)
-		
-		let request = Request(method: .post, uri: "\(host)/transmissions", headers: ["Content-Type": "application/json", "Authorization": apiKey], body: json.makeBody())
-		let response = try droplet?.client.respond(to: request)
-		
-		guard let statusCode = response?.status.statusCode, (200..<299).contains(statusCode) else {
-			throw Abort.serverError
-		}
-	}
-	
-	/**
-	Sends a request about forgot password to the user based on their email.
-	- Parameters: to - Sends it to the user
-	- Parameters: jwt - token expected
-	**/
-	public func sendForgotPasswordEmail(to user: User, jwt: String) throws {
-		// Generate Template
-		let template = HTMLTemplate.forgot(user: user, jwt: jwt)
-		let message = try HTMLTemplate.makeMessage(template)
-		let email = Email(emails: [user.email], subject: template.subject, message: message)
-		
-		var emailJSON = try email.makeJSON()
-		try emailJSON.set("html", message)
-		
-		var json = JSON()
-		try json.set("recipients", try email.makeEmailJSON())
-		try json.set("content", emailJSON)
-		
-		let request = Request(method: .post, uri: "\(host)/transmissions", headers: ["Content-Type": "application/json", "Authorization": apiKey], body: json.makeBody())
-		let response = try droplet?.client.respond(to: request)
-		
-		guard let statusCode = response?.status.statusCode, (200..<299).contains(statusCode) else {
-			throw Abort.serverError
-		}
-	}
-	
 	/**
 	Sends a verification email based on the parameters given
 	- Parameters: to - Sends it to the person
-	- Parameters: jwt - The token from the registration process
+	- Parameters: code - The token from the registration process
 	**/
-	public func sendVerificationEmail(to user: User, jwt: String) throws {
+	public func sendVerificationEmail(to user: User, code: Int) throws {
 		//: Generate the Email Message Format
-		let template = HTMLTemplate.verification(user: user, jwt: jwt)
+		let template = HTMLTemplate.verification(user: user, code: code)
 		let message = try HTMLTemplate.makeMessage(template)
 		let email = Email(emails: [user.email], subject: template.subject, message: message)
 		
