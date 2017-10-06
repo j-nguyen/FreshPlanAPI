cdf5962601cfe48c57deb4679f7f236aa113b337
Johnny Nguyen <jnguyen1236@gmail.com>
2017-10-06 00:20:16 -0400

    removed all emails

--- Sources/App/Utilities/Extensions/String+.swift
+++ Sources/App/Utilities/Extensions/String+.swift
@@ -6,14 +6,4 @@
 //
 
 extension String {
-	static func format(_ file: String,  _ args: Any...) -> String {
-		var index = 0
-		var result = file
-		while (result.contains("%@")) {
-			let range = result.range(of: "%@")
-			result = result.replacingOccurrences(of: "%@", with: "\(args[index])", options: .literal, range: range)
-			index += 1
-		}
-		return result
-	}
 }
