4ef54c8afb574d4c7e50695bafe10787448cf12d
Johnny Nguyen <jnguyen1236@gmail.com>
2017-11-04 21:59:46 -0400

    Added seed command and updated dependcies for swift 4

--- Package.swift
+++ Package.swift
@@ -9,9 +9,9 @@ let package = Package(
         .executable(name: "Run", targets: ["Run"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
-        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
-        .package(url: "https://github.com/vapor/jwt-provider.git", .upToNextMajor(from: "1.2.0")),
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.3.0")),
+        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.3.0")),
+        .package(url: "https://github.com/vapor/jwt-provider.git", .upToNextMajor(from: "1.3.0")),
         .package(url: "https://github.com/vapor-community/postgresql-provider.git", .upToNextMajor(from: "2.1.0")),
         .package(url: "https://github.com/vapor/validation.git", .upToNextMajor(from: "1.1.0"))
     ],
--- Sources/App/Commands/SeedCommand.swift
+++ Sources/App/Commands/SeedCommand.swift
@@ -32,9 +32,84 @@ public final class SeedCommand: Command {
 		}
 	}
 	
+	fileprivate func addUsers() throws {
+		for i in 1...25 {
+			let user = try User(
+				firstName: "Fake\(i)",
+				lastName: "User\(i)",
+				displayName: "fakeuser\(i)",
+				email: "fakeuser\(i)@example.com",
+				password: "test1234"
+			)
+			user.verified = true
+			console.print("Attempting to add \(user.displayName)")
+			try user.save()
+			console.print("User added.")
+		}
+		console.print("Added 25 users.")
+	}
+	
+	fileprivate func addFriends() throws {
+		// we will attempt to add every single friend, lol
+		for primaryId in 1...25 {
+			// we will add the !, because this is run after addUsers()
+			let user = try User.find(primaryId)!
+			for secondaryId in 1...25 {
+				let secondaryUser = try User.find(secondaryId)!
+				// attempt to add friend here
+				console.print("User: \(user.displayName) adding \(secondaryUser.displayName)")
+				let friend = Friend(userId: user.id!, friendsId: secondaryUser.id!)
+				try friend.save()
+				console.print("Friend added.")
+			}
+		}
+		console.print("Added all friends")
+	}
+	
+	fileprivate func addMeetups() throws {
+		for i in 1...5 {
+			let user = try User.find(i)!
+			for j in 1...3 {
+				for k in 1...5 {
+					let currentDate = Date()
+					let meetup = Meetup(
+						meetupTypeId: Identifier(j),
+						userId: user.id!,
+						title: "Meetup\(i)\(j)\(k)",
+						startDate: currentDate,
+						endDate: currentDate.addingTimeInterval(10293), // we'll add some random date
+						metadata: ""
+					)
+					console.print("Attempting to add meetup: \(meetup.title)")
+					try meetup.save()
+					console.print("Meetup saved")
+				}
+			}
+		}
+		console.print("Meetup saved")
+	}
+	
+	fileprivate func addInvites() throws {
+		for i in 1...5 {
+			let user = try User.find(i)!
+			for j in 1...10 {
+				let meetup = try Meetup.find(j)!
+				// attempt to get an invite here
+				let invite = Invitation(userId: user.id!, meetupId: meetup.id!)
+				console.print("Attempt to add invite: \(try invite.meetup.get()!.title)")
+				try invite.save()
+				console.print("Invitation saved")
+			}
+		}
+	}
+	
 	public func run(arguments: [String]) throws {
 		console.print("running custom command..")
 		try addMeetupTypes()
+		try addUsers()
+		try addFriends()
+		try addMeetups()
+		try addInvites()
 	}
 }
 
