b6d30bcd0e519f741be485bce7e99ccc8515c7c1
Johnny Nguyen <jnguyen1236@gmail.com>
2017-12-02 15:30:52 -0500

    Altered user controller and friends controller

--- Sources/App/Collections/V1Collection.swift
+++ Sources/App/Collections/V1Collection.swift
@@ -14,11 +14,10 @@ public final class V1Collection: EmptyInitializable, RouteCollection {
 	public func build(_ builder: RouteBuilder) throws {
 		// gets the versioning for future release
 		let api = builder.grouped("api", "v1")
-		
 		// add from the controllers
 		AuthController().addRoutes(api)
-		UserController().addRoutes(api)
-    
+    try api.grouped(TokenMiddleware()).resource("users", UserController.self)
+    try api.grouped(TokenMiddleware()).grouped("users").resource("friends", FriendController.self)
     try api.grouped(TokenMiddleware()).resource("meetup", MeetupController.self)
     try api.grouped(TokenMiddleware()).resource("invites", InviteController.self)
 	}
