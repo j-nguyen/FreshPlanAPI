4add838556c153fc0ecb9e9d8184ce04c52bbd47
Johnny Nguyen <jnguyen1236@gmail.com>
2017-12-08 20:47:59 -0500

    Fixed incase sensitive searching

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -17,7 +17,8 @@ public final class UserController: EmptyInitializable, ResourceRepresentable {
 		
 		// if a search query shows up, we can filter based on the contains
 		if let search = request.query?["search"]?.string {
-			let users = try User.makeQuery().filter("displayName", .contains, search.lowercased()).all()
+      let users = try User.makeQuery().filter("displayName", .custom("~*"), search).all()
+      
 			return try users.makeJSON()
 		}
 		
