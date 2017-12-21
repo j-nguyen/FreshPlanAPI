e6d55565ca205726ee0fe420582f1e1f74599573
Johnny Nguyen <jnguyen1236@gmail.com>
2017-12-21 13:05:46 -0500

    Setup migration and added unique constraint on invitation

---
+++ Sources/App/Models/Migration.swift
@@ -0,0 +1,20 @@
+//
+//  Migration.swift
+//  App
+//
+//  Created by Johnny Nguyen on 2017-12-21.
+//
+
+import Foundation
+import Vapor
+import FluentProvider
+
+public struct Migration: Preparation {
+  public static func prepare(_ database: Database) throws {
+    
+  }
+  
+  public static func revert(_ database: Database) throws {
+    
+  }
+}
