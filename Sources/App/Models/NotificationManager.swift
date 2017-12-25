c9cd3dc97f44e3b2743b8180262feb3d1c07c688
Johnny Nguyen <jnguyen1236@gmail.com>
2017-12-25 14:01:21 -0500

    Setup custom notification manager db model for managing notifications

---
+++ Sources/App/Models/NotificationManager.swift
@@ -0,0 +1,43 @@
+//
+//  Notification.swift
+//  App
+//
+//  Created by Johnny Nguyen on 2017-12-25.
+//
+
+import Foundation
+import Vapor
+import FluentProvider
+
+public final class NotificationManager: Model {
+  public var uuid: String
+  
+  public let storage: Storage = Storage()
+  
+  public init(uuid: String) {
+    self.uuid = uuid
+  }
+  
+  public init(row: Row) throws {
+    uuid = try row.get("uuid")
+  }
+  
+  public func makeRow() throws -> Row {
+    var row = Row()
+    try row.set("uuid", uuid)
+    return row
+  }
+}
+
+extension NotificationManager: Preparation {
+  public static func prepare(_ database: Database) throws {
+    try database.create(self) { db in
+      db.id()
+      db.custom("uuid", type: "TEXT")
+    }
+  }
+  
+  public static func revert(_ database: Database) throws {
+    try database.delete(self)
+  }
+}
